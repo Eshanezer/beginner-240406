74671119f49664a16227a1675d5959196f745a1c
Eshanezer <dilmina.eshan.d@outlook.com>
2024-04-06 18:18:53 +0300

    basic react project

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{ // change default server port to 3000
+    port:3000
+  }
 })
